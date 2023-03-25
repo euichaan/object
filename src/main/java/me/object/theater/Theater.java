5122ce99e88478c149000f7648be050904baeefc
euichanhwang <pine6971@naver.com>
2023-03-25 14:32:54 +0900

    refactor : Theater가 Audience와 TicketSeller에 관해 너무 세세한 부분까지 알지 못하도록 정보를 차단한다

--- src/main/java/me/object/theater/Theater.java
+++ src/main/java/me/object/theater/Theater.java
@@ -13,14 +13,6 @@ public class Theater {
 	}
 
 	public void enter(Audience audience) {
-		if (audience.getBag().hasInvitation()) { // 초대장이 있는 경우
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			audience.getBag().setTicket(ticket);
-		} else { // 초대장이 없는 경우. 티켓을 판매해야 한다.
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			audience.getBag().minusAmount(ticket.getFee());
-			ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-			audience.getBag().setTicket(ticket);
-		}
+		ticketSeller.sellTo(audience);
 	}
 }
--- src/main/java/me/object/theater/domain/TicketSeller.java
+++ src/main/java/me/object/theater/domain/TicketSeller.java
@@ -11,4 +11,16 @@ public class TicketSeller {
 	public TicketOffice getTicketOffice() {
 		return ticketOffice;
 	}
+
+	public void sellTo(Audience audience) {
+		if (audience.getBag().hasInvitation()) { // 초대장이 있는 경우
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().setTicket(ticket);
+		} else { // 초대장이 없는 경우. 티켓을 판매해야 한다.
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().minusAmount(ticket.getFee());
+			ticketOffice.plusAmount(ticket.getFee());
+			audience.getBag().setTicket(ticket);
+		}
+	}
 }
