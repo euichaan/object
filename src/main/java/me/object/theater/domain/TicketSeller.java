b4bb435d622755ab1304d09b62c68e403e006786
euichanhwang <pine6971@naver.com>
2023-03-25 14:46:24 +0900

    refactor : 캡슐화를 통해 결합도를 낮춘다

--- src/main/java/me/object/theater/domain/TicketSeller.java
+++ src/main/java/me/object/theater/domain/TicketSeller.java
@@ -2,25 +2,13 @@ package me.object.theater.domain;
 
 public class TicketSeller {
 
-	private TicketOffice ticketOffice;
+	private TicketOffice ticketOffice; // 자신이 일하는 매표소를 알고 있어야 한다.
 
 	public TicketSeller(TicketOffice ticketOffice) {
 		this.ticketOffice = ticketOffice;
 	}
 
-	public TicketOffice getTicketOffice() {
-		return ticketOffice;
-	}
-
 	public void sellTo(Audience audience) {
-		if (audience.getBag().hasInvitation()) { // 초대장이 있는 경우
-			Ticket ticket = ticketOffice.getTicket();
-			audience.getBag().setTicket(ticket);
-		} else { // 초대장이 없는 경우. 티켓을 판매해야 한다.
-			Ticket ticket = ticketOffice.getTicket();
-			audience.getBag().minusAmount(ticket.getFee());
-			ticketOffice.plusAmount(ticket.getFee());
-			audience.getBag().setTicket(ticket);
-		}
+		ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
 	}
 }
