72452095d921ab0ba93f1eca1aa781a4bab4f970
euichanhwang <pine6971@naver.com>
2023-03-26 19:53:43 +0900

    refactor : 캡슐화를 통해 Bag을 자율적인 존재로 변경한다

--- src/main/java/me/object/theater/domain/Bag.java
+++ src/main/java/me/object/theater/domain/Bag.java
@@ -2,6 +2,10 @@ package me.object.theater.domain;
 
 public class Bag {
 
+	private Long amount;
+	private Invitation invitation;
+	private Ticket ticket;
+
 	public Bag(long amount) {
 		this(null, amount); // 이벤트 미당첨 : 현금
 	}
@@ -11,28 +15,27 @@ public class Bag {
 		this.amount = amount;
 	}
 
-	private Long amount;
-	private Invitation invitation;
-	private Ticket ticket;
+	public Long hold(Ticket ticket) {
+		if (hasInvitation()) {
+			setTicket(ticket);
+			return 0L;
+		} else {
+			setTicket(ticket);
+			minusAmount(ticket.getFee());
+			return ticket.getFee();
+		}
+	}
 
-	public boolean hasInvitation() {
+	private boolean hasInvitation() {
 		return invitation != null;
 	}
 
-	public boolean hasTicket() {
-		return ticket != null;
-	}
-
-	public void setTicket(Ticket ticket) {
+	private void setTicket(Ticket ticket) {
 		// 초대장을 티켓으로 교환한다
 		this.ticket = ticket;
 	}
 
-	public void minusAmount(Long amount) {
+	private void minusAmount(Long amount) {
 		this.amount -= amount;
-	}
-
-	public void plusAmount(Long amount) {
-		this.amount += amount;
 	}
 }
